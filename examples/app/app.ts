6fe56418f1f4246adc9fc43a1f7e74be46fb83cf
samhuk <samhughes1701@hotmail.co.uk>
2023-07-09 13:44:36 +0100

    tidy up combination of app and error-app. Error code still imperfect.

---
+++ examples/app/app.ts
@@ -0,0 +1,48 @@
+import { createGFError } from 'good-flow'
+
+import { connectToDb } from './db'
+import { doJobs } from './jobs'
+import { healthcheckAPI } from './api'
+import log from './log'
+import { tearDownEnv } from './teardown'
+import { validateEnv } from './env'
+
+const createMockMetrics = (jobs: string[]) => jobs.map(jobName => ({
+  'Job Name': jobName,
+  'Time Taken (s)': (Math.random() * 5).toFixed(1),
+  'People Encountered': Math.round(Math.random() * 5),
+}))
+
+const splash = () => {
+  log.log(f => f.italic(f.bold('-- Example App --')))
+  log.spacer()
+}
+
+export const app = async (jobs: string[]) => {
+  try {
+    splash()
+
+    log.step({ msg: 'Starting app.', code: 'INIT' })
+
+    validateEnv()
+
+    await connectToDb()
+    await healthcheckAPI()
+    await doJobs(jobs)
+    await tearDownEnv()
+
+    log.spacer()
+    log.table(createMockMetrics(jobs))
+    log.spacer()
+    log.success(c => c.green(c.bold('Done!')))
+  }
+  catch (e) {
+    createGFError({
+      msg: 'An error occured.',
+      inner: e as any,
+    }).log()
+    process.exit(1)
+  }
+
+  process.exit(0)
+}
--- examples/app/index.ts
+++ examples/app/index.ts
@@ -1,59 +1,10 @@
-import { createGFError } from 'good-flow'
+import { app } from './app'
 
-import { connectToDb } from './db'
-import { doJobs } from './jobs'
-import { healthcheckAPI } from './api'
-import log from './log'
-import { tearDownEnv } from './teardown'
-import { validateEnv } from './env'
-
-const JOBS = [
+app([
   'open_garage',
   'get_in_car',
   'start_engine',
   'drive_to_work',
   'do_work',
   'drive_home',
-]
-
-const createMockMetrics = () => JOBS.map(jobName => ({
-  'Job Name': jobName,
-  'Time Taken (s)': (Math.random() * 5).toFixed(1),
-  'People Encountered': Math.round(Math.random() * 5),
-}))
-
-const splash = () => {
-  log.log(f => f.italic(f.bold('-- Example App --')))
-  log.spacer()
-}
-
-const main = async () => {
-  try {
-    splash()
-
-    log.step({ msg: 'Starting app.', code: 'INIT' })
-
-    validateEnv()
-
-    await connectToDb()
-    await healthcheckAPI()
-    await doJobs(JOBS)
-    await tearDownEnv()
-
-    log.spacer()
-    log.table(createMockMetrics())
-    log.spacer()
-    log.success(c => c.green(c.bold('Done!')))
-  }
-  catch (e) {
-    createGFError({
-      msg: 'An error occured.',
-      inner: e as any,
-    }).log()
-    process.exit(1)
-  }
-
-  process.exit(0)
-}
-
-main()
+])
